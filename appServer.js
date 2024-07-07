c6b77edeeee2f5d85fb0e86d402aad5cbfffa84e
Benson-Ogheneochuko <ob.ogheneochuko@gmail.com>
2024-07-08 00:26:23 +0100

    fix vercel.json file

--- appServer.js
+++ appServer.js
@@ -1,38 +1,20 @@
 import './env.js'
 import { app } from "./index.js";
-import { connnectDatabase, closeDatabase} from "./postgressDB/connectDB.js";
+import { connnectDatabase, closeDatabase } from "./postgressDB/connectDB.js";
 
-const PORT = process.env.PORT || 3000;
+// Initialize database connection
+connnectDatabase().catch(console.error);
 
-async function startServer() {
-  const maxRetries = 5;
-  let retries = 1;
-  while (retries <= maxRetries) {
-    try {
-      const dbConnected = await connnectDatabase();
-      !dbConnected
-        ? console.log(
-            "Server startup aborted due to database connection failure"
-          )
-        : app.listen(PORT, () => {
-            console.log(
-              `Database connection successful \n Server running on port ${PORT}`
-            );
-          });
-      return;
-    } catch (err) {
-      console.log(
-        `Database connection failed. Retries left: ${maxRetries - retries}`
-      );
-      retries += 1;
-      await new Promise((res) => setTimeout(res, 5000));
-    }
-  }
-  console.log(`Failed to connect to the database after 5 attempts`);
-  return false;
+// Export the Express app as a module
+export default app;
+
+// If running locally
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
-
-startServer();
 
 process.on("SIGINT", async () => {
   console.log("Shutting down gracefully");
